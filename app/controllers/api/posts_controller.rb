a4f337626771ce83f8326dcf00824d19d9822019
anikonchuk <anikonch@gmail.com>
2019-01-10 13:57:39 -0500

    Add create method and post_params method to posts_controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -5,11 +5,26 @@ class Api::PostsController < ApplicationController
     render json: Post.all
   end
 
+  def show
+    render json: @post
+  end
 
+  def create
+    post = Post.new(post_params)
+    if post.save
+      render json: post
+    else
+      render json: { message: post.errors }, status: 400
+    end
+  end
 
   private
 
     def set_post
       @post = Post.find_by(id: params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :content, :upvotes, :user_id)
     end
 end
