0588e3f8b5fe07551d4b7541e8c9ebc90630f82c
anikonchuk <anikonch@gmail.com>
2019-01-10 14:24:35 -0500

    Add routes for users and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :posts, except: [:new, :edit]
+    resources :users, only: [:create]
+    resources :sessions, only: [:create, :destroy]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
