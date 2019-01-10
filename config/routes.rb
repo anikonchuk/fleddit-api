ae15a476b899dbcfb8c6a95944a47bcb57d90fb6
anikonchuk <anikonch@gmail.com>
2019-01-10 13:46:53 -0500

    Add routes for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    resources :posts, except: [:new, :edit]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
