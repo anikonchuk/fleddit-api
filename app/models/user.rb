4fc15908319f1b4e13fd9826b2830cb0d7757b8e
anikonchuk <anikonch@gmail.com>
2019-01-10 13:35:02 -0500

    Add relations, has_secure_password, and validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_secure_password
+
+  validates :username, presence: true
+  validates :username, uniqueness: { case_sensitive: false }
+
 end
