1fddd6aba0d8d9727eb4158bc3bce87201c08631
anikonchuk <anikonch@gmail.com>
2019-01-14 08:47:06 -0500

    Remove user model, add associations to comment and post model

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  has_many :posts
-  has_secure_password
-
-  validates :username, presence: true
-  validates :username, uniqueness: { case_sensitive: false }
-
-end
