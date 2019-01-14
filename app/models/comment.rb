75748de5e430fe01a887dbd3beddce0a5e22c5f5
anikonchuk <anikonch@gmail.com>
2019-01-14 09:02:06 -0500

    Revise validations on post and comment models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :post
+
+  validates :content, :author, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_many :comments
 
-  validates :title, :content, presence: true
+  validates :title, :content, :author, presence: true
 
 end
