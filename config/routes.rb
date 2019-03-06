954af2ae81669eca517a97559cacf82760ff5966
anikonchuk <anikonch@gmail.com>
2019-03-06 09:15:28 -0500

    Add update to routes for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   namespace :api do
-    resources :posts, only: [:show, :index, :create]
-    resources :comments, only: [:index, :create]  
+    resources :posts, only: [:show, :index, :create, :update]
+    resources :comments, only: [:index, :create]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
