3943f66b61e15031db594ed380ecb21aad45cd39
anikonchuk <anikonch@gmail.com>
2019-03-13 11:08:08 -0400

    Add :created_at to post_serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,3 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :img_url, :author, :likes
+  attributes :id, :title, :content, :img_url, :author, :likes, :created_at
 end
