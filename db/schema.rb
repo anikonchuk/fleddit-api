783fb442db8a6d73ac32c35dfca7c7ce906d21bc
anikonchuk <anikonch@gmail.com>
2019-03-21 11:30:34 -0400

    Add migrations to add user_id to posts and comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_21_145613) do
+ActiveRecord::Schema.define(version: 2019_03_21_152930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_03_21_145613) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "content"
+    t.integer "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
@@ -32,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_03_21_145613) do
     t.string "img_url", default: ""
     t.string "author"
     t.integer "likes", default: 0
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
