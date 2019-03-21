16f088652e0c3c5bc16cc9fc830f389e82a1b2b0
anikonchuk <anikonch@gmail.com>
2019-03-21 11:27:15 -0400

    Add relationships to user, post, comment models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
+  belongs_to :user
 
   validates :content, :author, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true, uniqueness: true
 
+  has_many :posts
+  has_many :comments
+
 end
