1f061adcbdb1d6164b7ca32d08bf4ce79c180da6
anikonchuk <anikonch@gmail.com>
2019-03-21 10:57:55 -0400

    Add user model, database migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_06_141256) do
+ActiveRecord::Schema.define(version: 2019_03_21_145613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 2019_03_06_141256) do
     t.string "img_url", default: ""
     t.string "author"
     t.integer "likes", default: 0
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
