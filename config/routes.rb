5c5559aed6d16510d16bde762baa0ab9625bcb0c
anikonchuk <anikonch@gmail.com>
2019-03-21 10:24:25 -0400

    Update routes to add users and auth/login routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     resources :posts, only: [:show, :index, :create, :update]
     resources :comments, only: [:index, :create]
+    resources :users, param: :_username
+    post '/auth/login', to: 'authentication#login'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
