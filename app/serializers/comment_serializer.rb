ed8b877b172df376b32f11e28aa93565337d875a
anikonchuk <anikonch@gmail.com>
2019-03-26 09:01:37 -0400

    Add relationship to user to post and comment serializers

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,3 +1,4 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :content, :post_id, :author
+  belongs_to :user
 end
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,4 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :content, :img_url, :author, :likes, :created_at
+  belongs_to :user
 end
