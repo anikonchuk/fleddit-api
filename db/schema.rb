1f8b3c51071bf4d0c74d7b5d3b1a675e0300653d
anikonchuk <anikonch@gmail.com>
2019-03-26 09:04:08 -0400

    Remove author from posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_21_152930) do
+ActiveRecord::Schema.define(version: 2019_03_26_130326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2019_03_21_152930) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "img_url", default: ""
-    t.string "author"
     t.integer "likes", default: 0
     t.integer "user_id"
   end
