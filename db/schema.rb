5a4155569e9ecc35e97914d32fcd7c352eb83d91
anikonchuk <anikonch@gmail.com>
2019-03-26 09:05:06 -0400

    Remove author from comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_26_130326) do
+ActiveRecord::Schema.define(version: 2019_03_26_130433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string "author"
     t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
